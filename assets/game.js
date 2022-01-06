7a5892aae381e6752519d29581525b620f555864
NumiBlue <thekindlyblog@gmail.com>
2022-01-06 13:34:43 -0600

    initial game setup

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -13,9 +14,13 @@ var enemyAttack = 12;
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-enemyHealth = enemyHealth - playerAttack;
+ // if player choses to fight, fight
+ if (promptFight === "fight" || promptFight === "FIGHT") {
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth = enemyHealth - playerAttack;
 console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
@@ -30,7 +35,21 @@ console.log(
 playerHealth = playerHealth - enemyAttack;
 console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 );
+//if player chooses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    //confirm player wants to skip
+    confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
+    //if yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from player for skipping
+        playerMoney =playerMoney - 2;
+    }
+    //if no (false), ask question by running fight() again
+   else {
+       fight();
+   }
 // check player's health
 if (playerHealth <= 0) {
   window.alert(playerName + " has died!");
@@ -38,4 +57,10 @@ if (playerHealth <= 0) {
 else {
   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
-}
+//if player did not choose 1 or 2 in prompt
+
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+ }
+};
+
